eb2254a4d30e5d18dbb855af7f1a80911bd9f5f2
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-29 16:34:00 +0000

    finally done

--- answer.php
+++ answer.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-yellow.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-amber.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
@@ -21,7 +21,6 @@
 
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <script src="./js/script.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
@@ -30,26 +29,32 @@
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/ reverse a numebr.jpeg" alt="reverse number image" />
+        <img src="./images/ reverse a numebr.jpeg" alt="reverse a number image" width="300" />
       </div>
-      <form action="./answer.php" method="GET">
-        <br />
-        <div class="page-content">
-        </div>
-        <div class="mdl-textfield mdl-js-textfield">
-          <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="number-one" />
-          <label class="mdl-textfield__label" for="number-one">Number here...</label>
-          <span class="mdl-textfield__error">Input is not a number!</span>
-        </div>
-        <br />
-        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-          type="submit">
-          REVERSE
-        </button>
-      </form>
-      <br />
       <div class="page-content-answer">
-        <div id="answer"></div>
+        <div id="answer">
+          <?php
+          // input
+          $inputNumber = $_GET["number-one"];
+          $reversed = '';
+
+          // process
+          $length = strlen($inputNumber);
+
+          while ($length > 0) {
+            $lastDigit = substr($inputNumber, $length - 1, 1);
+            $reversed = $reversed . $lastDigit;
+            $length = $length - 1;
+          }
+
+          // output
+          echo 'Your number was: ' . $inputNumber . '.<br>';
+          echo 'Your reversed number is: ' . $reversed . '<br>';
+          ?>
+        </div>
+      </div>
+      <div class="page-content-return">
+        <a href="./index.php">Return ...</a>
       </div>
     </main>
   </div>
